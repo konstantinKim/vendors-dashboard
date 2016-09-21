3bae508d80f1dc33ed05e7f6c103eb6f4c7c52e1
Konstantin <kim.vkk@gmail.com>
2016-09-21 14:35:00 +0500

    fix

--- src/config/settings.js
+++
@@ -1,5 +0,0 @@
-export const BACKEND_HOST = '/api/v1/'
-export const IMG_HOST = ''//'http://vd.test'
-const token = localStorage.getItem('token')  
-export const REQUEST_HEADERS = {'Accept': 'application/json','Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
-export const TOKEN = token
