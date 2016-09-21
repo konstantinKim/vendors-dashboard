d3de3ad13e42947e2535695006285efdefdd52c3
Konstantin <kim.vkk@gmail.com>
2016-09-21 11:46:21 +0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var autoprefixer = require('autoprefixer');
 var precss = require('precss');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  //devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
     'babel-polyfill',
